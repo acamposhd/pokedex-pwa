d34632f49c2354af4da1abd9a9e94998a440dee6
Alberto Campos <alberto.campos@heyirys.com>
2022-01-20 21:44:52 -0600

    Feature: coverted into pwa

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pokemon, setPokemon] = useState({});
+  const [currentPokemonId, setCurrentPokemonId] = useState(1);
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  const fetchPokemon = (id) => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => response.json())
+      .then((data) => setPokemon(data));
+  };
+
+  useEffect(() => {
+    console.log({ pokemon });
+    pokemon?.abilities?.map((ability) => console.log(ability.ability.name));
+    setCurrentPokemonId(pokemon.id);
+  }, [pokemon]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="flex-container">
+          <img
+            src={
+              pokemon?.sprites?.front_default ??
+              "https://pngimg.com/uploads/pokeball/pokeball_PNG20.png"
+            }
+            className="poke-image"
+            alt="logo"
+          />
+          <img
+            src={
+              pokemon?.sprites?.back_default ??
+              "https://webstockreview.net/images/pokeball-clipart-pdf-16.png"
+            }
+            className="poke-image"
+            alt="logo"
+          />
+        </div>
+        <p className="poke-title">{pokemon.name} </p>
+        <div className="flex-container">
+          <button
+            className="button"
+            onClick={() => fetchPokemon(currentPokemonId - 1)}
+          >
+            Back
+          </button>
+          <button
+            className="button"
+            onClick={() => fetchPokemon(getRandomInt(1, 500))}
+          >
+            Random
+          </button>
+          <button
+            className="button"
+            onClick={() => fetchPokemon(currentPokemonId + 1)}
+          >
+            Next
+          </button>
+        </div>
       </header>
     </div>
   );
